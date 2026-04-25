7679f4500c5905dbc1bfbf803d4cee3684576210
Awais Hashar <awais.hashar@gmail.com>
2026-04-25 18:49:52 -0400

    resolved upload error

--- apps/api/config/initializers/active_storage.rb
+++ apps/api/config/initializers/active_storage.rb
@@ -1,4 +1,4 @@
-Rails.application.config.active_storage.service = ENV.fetch("ACTIVE_STORAGE_SERVICE", "local").to_sym
+configured_service = ENV["ACTIVE_STORAGE_SERVICE"].to_s.strip
+Rails.application.config.active_storage.service = configured_service.to_sym if configured_service.present?
 Rails.application.config.active_storage.variant_processor = :vips
 Rails.application.config.active_storage.routes_prefix = "/storage"
-
